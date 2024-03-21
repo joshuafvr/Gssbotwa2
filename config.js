52fd175614f2a8b9194fe74f63d3223b8e67cd6c
joshuafvr <74902842+joshuafvr@users.noreply.github.com>
2024-03-21 17:47:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = 'unbothered';
+global.owner = ['9021325614'];
+global.premium = ['9021325614'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
 global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
 global.sessionName = 'session';
